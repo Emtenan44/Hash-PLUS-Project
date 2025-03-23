feaa866f1d9180c6041fda975a0329d8f6d669f6
Emtenan44 <emtenan.haitham@gmail.com>
2025-03-23 19:17:38 +0300

    Hash Plus

---
+++ script.js
@@ -0,0 +1,14 @@
+const menuIcon = document.querySelector('.menu-icon');
+
+const offscreenMenu = document.querySelector('.off-screen-menu');
+
+menuIcon.addEventListener('click', () => {
+  menuIcon.classList.toggle('active');
+  offscreenMenu.classList.toggle('active');
+})
+
+
+document.querySelector(".contact form").addEventListener("submit", function(event) {
+  event.preventDefault(); // يمنع إعادة تحميل الصفحة
+  this.reset(); // يمسح البيانات في الفورم
+});
